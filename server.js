9f1e7ae536427f128a7754f45a3482e2c6a375bb
Alex Kovtunov <1sasha.sasha1@gmail.com>
2020-07-06 22:42:02 +0200

    [fix] Fixed servers config to read path variable

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Pusher = require('pusher');
 
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 const pusher = new Pusher({
