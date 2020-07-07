d24c31fd172ca8c78913cd5cb974bca5aa9e2b99
Alex Kovtunov <1sasha.sasha1@gmail.com>
2020-07-07 02:57:59 +0200

    [feature] Added working synchronization of users and nicknames + colors

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Whiteboard from './components/Whiteboard';
-
 import Pusher from "pusher-js";
 import shortid from 'shortid'; // for generating unique IDs
+import Faker from 'faker'
 
-const channel_name =  'alex';//shortid.generate();
+
+const channel_name =  'P5Mil9LGK';//shortid.generate();
 const BASE_URL = process.env.REACT_APP_SERVER_URL;
+const username = Faker.internet.userName();
+const usercolor = Faker.internet.color();
 
 class App extends Component {
+
+  state = {
+    clients: new Set()
+  }
+
   constructor(props){
     super(props);
     // this.pusher = new Pusher({
@@ -20,6 +28,7 @@ class App extends Component {
     // });
     // Enable pusher logging - don't include this in production
     // Pusher.logToConsole = true;
+
     console.log(process.env)
     console.log(process.env.REACT_APP_PUSHER_APP_KEY)
     this.pusher = new Pusher(process.env.REACT_APP_PUSHER_APP_KEY, {
@@ -34,8 +43,17 @@ class App extends Component {
     });
 
     this.group_channel.bind("pusher:subscription_succeeded", () => {
-      console.log("subscription to group succeeded")
+      console.log("subscription to group succeeded");
+      this.group_channel.trigger('client-subscribed', {username: username, color: usercolor});
     });
+
+    this.group_channel.bind("client-subscribed", (data) => {
+      console.log("new client joined");
+      let clients = this.state.clients
+      clients.add(data)
+      this.setState({clients: clients})
+    });
+
   }
 
   render() {
@@ -44,21 +62,21 @@ class App extends Component {
         <div className="row">
           <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
             <div className="sidebar-header">
-              <h3>Whiteboard room</h3>
+              <h3>Whiteboard room {channel_name}</h3>
             </div>
 
             <ul className="list-unstyled components">
               <p>Color Guide</p>
               <li>
-                <div className="user user">User</div>
+                <div className="user" style={{background: usercolor}}>{username}</div>
               </li>
               <li>
-                <div className="user guest">Guest</div>
+                <div className="user">Guest</div>
               </li>
             </ul>
           </nav>
           <div className="col-md-9 col-lg-10 d-md-block bg-light">
-            <Whiteboard group_channel={this.group_channel}/>
+            <Whiteboard group_channel={this.group_channel} usercolor={usercolor}/>
           </div>
         </div>
       </div>
--- client/src/components/Whiteboard.js
+++ client/src/components/Whiteboard.js
@@ -11,6 +11,41 @@ class Whiteboard extends Component {
     messages: []
   }
 
+  async componentDidMount() {
+
+    let textGatherer = this._gatherText();
+
+    this.props.group_channel.bind('client-whiteboard-updated', (payload) => {
+      textGatherer(payload.data);
+      console.log('whiteboard was updated')
+      if (payload.is_final) {
+        const full_payload = textGatherer(); // get the gathered text
+        let obj = '';
+        if (full_payload) {
+          obj = JSON.parse(full_payload);
+
+          if (payload.id) {
+            Object.assign(obj, { id: payload.id, sender: payload.sender });
+          } else {
+            Object.assign(obj, { sender: payload.sender });
+          }
+        }
+
+        if (payload.action === 'add') {
+          this._sketch.addObject(JSON.stringify(obj));
+        } else if(payload.action === 'update') {
+          this._sketch.modifyObject(JSON.stringify(obj));
+        } else if(payload.action === 'remove') {
+          this._sketch.setSelected(payload.id);
+          this._sketch.removeSelected();
+        }
+
+        textGatherer = this._gatherText(); // reset to an empty string
+      }
+
+    });
+  }
+
   render() {
     return (
       <SketchField
@@ -19,7 +54,7 @@ class Whiteboard extends Component {
         width='1024px'
         height='768px'
         tool={this.state.tool}
-        lineColor='#EE92C2'
+        lineColor={this.props.usercolor}
         lineWidth={3}
         onUpdate={this.sketchUpdated}
         username='xx'
@@ -27,15 +62,42 @@ class Whiteboard extends Component {
     )
   }
 
-
+  _gatherText = () => {
+    let sentence = '';
+    return (txt = '') => {
+     return sentence += txt;
+    }
+  }
 
   sketchUpdated = (obj, action, sender, id = null) => {
-    console.log(this.props.group_channel)
-    this.props.group_channel.trigger('client-whiteboard-updated', id);
-    console.log(obj)
-    console.log(action)
-    console.log(sender)
-    console.log(id)
+    if (this.props.usercolor) {
+
+        let length_per_part = 8000; // maximum number of characters that can be alloted to a FabricJS object
+        let loop_count = Math.ceil(obj.length / length_per_part);
+
+        let from_str_index = 0;
+        for (let x = 0; x < loop_count; x++) {
+          const str_part = obj.substr(from_str_index, length_per_part);
+
+          const payload = {
+            action: action,
+            id: id,
+            data: str_part,
+            sender: this.state.usercolor
+          };
+
+          if (x + 1 === loop_count) { // if this is the final part
+            Object.assign(payload, { is_final: true });
+          }
+
+          this.updateOtherUsers(payload);
+          from_str_index += length_per_part;
+        }
+      }
+  }
+
+  updateOtherUsers = (payload) => {
+    this.props.group_channel.trigger('client-whiteboard-updated', payload);
   }
 }
 
