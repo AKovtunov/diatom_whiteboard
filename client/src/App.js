f5badf5a471f278bb389694f256b775dad89348d
Alex Kovtunov <1sasha.sasha1@gmail.com>
2020-07-23 02:13:36 +0200

    feat(WHIT-10): Participant should be able to select a tool to draw

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import Whiteboard from './components/Whiteboard';
 import UserList from './components/UserList';
 import Pusher from "pusher-js";
 import shortid from 'shortid'; // for generating unique IDs
 import Faker from 'faker'
+import { SliderPicker } from 'react-color';
 
 
 const channel_name =  'P5Mil9LGK';//shortid.generate();
 const BASE_URL = process.env.REACT_APP_SERVER_URL;
 const username = Faker.internet.userName();
-const usercolor = Faker.internet.color();
 
 class App extends Component {
 
@@ -19,7 +19,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      clients: {}
+      clients: {},
+      color: Faker.internet.color()
     }
 
     this.pusher = new Pusher(process.env.REACT_APP_PUSHER_APP_KEY, {
@@ -29,13 +30,17 @@ class App extends Component {
     });
 
     this.group_channel = this.pusher.subscribe(`private-group-${channel_name}`);
+  }
+
+  async componentDidMount() {
+    console.warn("COMPONENT componentDidMount FOR APP>JS");
     this.group_channel.bind("pusher:subscription_error", (status) => {
       console.log("error subscribing to group channel: ", status);
     });
 
     this.group_channel.bind("pusher:subscription_succeeded", () => {
       console.log("subscription to group succeeded");
-      this.group_channel.trigger('client-subscribed', {username: username, color: usercolor});
+      this.group_channel.trigger('client-subscribed', {username: username, color: this.state.color});
     });
 
     this.group_channel.bind("client-subscribed", (data) => {
@@ -52,18 +57,17 @@ class App extends Component {
 
     this.group_channel.bind("clients-refreshed", () => {
       console.log("Refreshed clients list, subscribing back")
-      console.log(`My name is ${username} and color is ${usercolor}`)
+      console.log(`My name is ${username} and color is ${this.state.color}`)
       const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ username: username, usercolor: usercolor })
+          body: JSON.stringify({ username: username, usercolor: this.state.color })
       };
+      console.log(`Sending { username: ${username}, usercolor: ${this.state.color} } to the server`)
       fetch('/clients', requestOptions)
           .then((response) => response.json())
           .then((data) => {
             this.setState({ clients: data })
-            console.log("state updated")
-            console.log(data)
           })
           .catch((error) => {
             console.error(error);
@@ -72,35 +76,38 @@ class App extends Component {
           console.log(this.state.clients)
     });
 
-  }
 
-  async componentDidMount() {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: username, usercolor: usercolor })
+        body: JSON.stringify({ username: username, usercolor: this.state.color })
     };
     fetch('/clients', requestOptions)
         .then((response) => response.json())
         .then((data) => {
           this.setState({ clients: data })
-          console.log("state updated")
-          console.log(data)
+          console.log("state is:")
+          console.log(this.state.clients)
         })
         .catch((error) => {
           console.error(error);
         });
-        console.log("state is:")
-        console.log(this.state.clients)
+  }
+
+  handleChange = (color, event) => {
+    this.setState({color: color.hex})
   }
 
   render() {
     return (
       <div className="container-fluid">
-        <div className="row">
-          <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div className="row logo-navbar">
+          <img src={logo} alt="DiatomEnterprises" className="logo" />
+        </div>
+        <div className="row main-container">
+          <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar">
             <div className="sidebar-header">
-              <h3>Whiteboard room {channel_name}</h3>
+              <p className="room-title">Whiteboard room {channel_name}</p>
             </div>
 
             <ul className="list-unstyled components">
@@ -108,8 +115,16 @@ class App extends Component {
               <UserList clients={this.state.clients} me={username}/>
             </ul>
           </nav>
-          <div className="col-md-9 col-lg-10 d-md-block bg-light">
-            <Whiteboard group_channel={this.group_channel} usercolor={usercolor}/>
+          <div className="col-md-8 col-lg-10 d-md-block bg-light whiteboard">
+            <Whiteboard group_channel={this.group_channel} username = {username} usercolor={this.state.color}/>
+          </div>
+        </div>
+        <div className="row bottom-bar">
+          <div className="col-md-12 col-lg-12 d-md-block bg-light">
+            <SliderPicker
+              color={ this.state.color }
+              onChangeComplete={ this.handleChange }
+            />
           </div>
         </div>
       </div>
